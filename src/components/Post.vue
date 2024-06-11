4858f72c77ddf7d8b95516b347317a15d54cabd2
Alethepata <alessia.patane@gmail.com>
2024-06-11 12:41:17 +0200

    add post

---
+++ src/components/Post.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: 'Posts',
+    props: {
+        title : String,
+        content: String,
+        image: String,
+        tags: Array,
+        category: String
+    }
+
+}
+</script>
+
+<template>
+    <div class="post-title">
+        <h1 class="text-center">{{ title }}</h1>
+    </div>
+    <div class="post-content d-flex">
+        <p>{{ content }}</p>
+        <div v-for="tag in tags" :key="tag.id" class="post-tags mx-2">
+            <p>#{{ tag?.name }}</p>
+        </div>
+    </div>
+    <div v-if="category" class="post-category">
+        <p>Category : {{ category.name }}</p>
+    </div>
+    <div v-if="image" class="post-img">
+        <img :src="image" :alt="title">
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,47 @@
 <script>
+
+import axios from 'axios';
+import { store } from '../data/store.js';
+
+import Post from '../components/Post.vue';
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        Post
+    },
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        getApi(endpoint) {
+            axios.get(endpoint)
+            .then(res => {
+            store.posts = res.data;
+        console.log(store.posts)
+            })
+        }
+    },
+    mounted() {
+        this.getApi(store.apiUrl + "/posts")
+    }
 }
 </script>
 
 <template>
-  <h1>home</h1>
+    <div class="container-posts m-5">
+        <div v-for="post in store.posts" :key="post.id" class="post my-3">
+            <Post 
+            :title="post.title" 
+            :content="post.content"
+            :image="post.image"
+            :tags="post.tags"
+            :category="post.category"
+            />
+        </div>
+    </div>
 </template>
 
 
