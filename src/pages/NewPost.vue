92540ca5466f0064fb53876853f510178b0b0b34
Alethepata <alessia.patane@gmail.com>
2024-06-11 19:48:29 +0200

    edit form

--- src/pages/NewPost.vue
+++ src/pages/NewPost.vue
@@ -64,50 +64,53 @@ export default {
     <div class="container">
         <div v-if="!this.success" class="container-form">
             <h1>New Post</h1>
-            <form>
-                <div class="mb-3">
-                    <label for="title" class="form-label">Title</label>
-                    <input v-model="this.title" type="title" class="form-control" id="title" name="title" />
-                </div>
+            <div class="form">
+                <form>
     
-                <div class="mb-3">
-                    <label for="content" class="form-label">Content</label>
-                    <input v-model="this.content" type="content" class="form-control" id="content" />
-                </div>
-
-                <div class="mb-3">
-                    <div class="form-check">
-                      <input class="form-check-input" v-model="this.published" v-bind:value="'true'" type="radio" name="flexRadioDefault" id="true">
-                      <label class="form-check-label" for="true">
-                        Pubblicato
-                      </label>
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Title</label>
+                        <input v-model="this.title" type="title" class="form-control" id="title" name="title" />
                     </div>
-                    <div class="form-check">
-                      <input class="form-check-input" v-model="this.published" v-bind:value="'false'" type="radio" name="flexRadioDefault" id="false">
-                      <label class="form-check-label" for="false">
-                        Non pubblicato
-                      </label>
+        
+                    <div class="mb-3">
+                        <label for="content" class="form-label">Content</label>
+                        <textarea v-model="this.content" class="form-control" id="content"></textarea>
                     </div>
-                </div>
     
-                <div class="mb-3">
-                    <select v-model="this.category" class="form-select" aria-label="Default select example">
-                        <option selected>Seleziona la categoria</option>
-                        <option v-for="category in store.categories" :key="category.id" :value="category.id">{{category.name}}</option>
-                    </select>
-                </div>
-
-                <div class="mb-3 d-flex flex-wrap justify-content-center gap-3">
-                    <div v-for="tag in store.tags" :key="tag.id" class="form-check">
-                      <input v-model="this.tags" v-bind:value="tag.id" class="form-check-input" type="checkbox" :value="tag.id" id="flexCheckDefault">
-                      <label class="form-check-label" for="flexCheckDefault">
-                        {{tag.name}}
-                      </label>
+                    <div class="mb-3">
+                        <div class="form-check">
+                          <input class="form-check-input" v-model="this.published" v-bind:value="'true'" type="radio" name="flexRadioDefault" id="true">
+                          <label class="form-check-label" for="true">
+                            Pubblicato
+                          </label>
+                        </div>
+                        <div class="form-check">
+                          <input class="form-check-input" v-model="this.published" v-bind:value="'false'" type="radio" name="flexRadioDefault" id="false">
+                          <label class="form-check-label" for="false">
+                            Non pubblicato
+                          </label>
+                        </div>
                     </div>
-                </div>
-                
-                </form>
-            <button @click="this.getForm" class="btn btn-primary button">Submit</button>
+        
+                    <div class="mb-3">
+                        <select v-model="this.category" class="form-select" aria-label="Default select example">
+                            <option selected>Seleziona la categoria</option>
+                            <option v-for="category in store.categories" :key="category.id" :value="category.id">{{category.name}}</option>
+                        </select>
+                    </div>
+    
+                    <div class="mb-3 d-flex flex-wrap justify-content-center gap-3">
+                        <div v-for="tag in store.tags" :key="tag.id" class="form-check">
+                          <input v-model="this.tags" v-bind:value="tag.id" class="form-check-input" type="checkbox" :value="tag.id" id="flexCheckDefault">
+                          <label class="form-check-label" for="flexCheckDefault">
+                            {{tag.name}}
+                          </label>
+                        </div>
+                    </div>
+                    
+                    </form>
+                <button @click="this.getForm" class="btn btn-primary button">Submit</button>
+            </div>
         </div>
         <div v-if="this.success" class="success">
             <h1>Post creato con successo</h1>
@@ -117,5 +120,9 @@ export default {
 
 
 <style>
+.form{
+    width: 60%;
+    margin: 0 auto;
 
+}
 </style>
