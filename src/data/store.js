f3fdcdb989836da1652969536e23c11d613b9d39
Alethepata <alessia.patane@gmail.com>
2024-06-11 18:17:13 +0200

    add categories end tags

--- src/data/store.js
+++ src/data/store.js
@@ -3,4 +3,6 @@ import { reactive } from "vue";
 export const store = reactive({
     apiUrl: 'http://localhost:3000',
     posts: [],
+    categories: [],
+    tags: []
 });
--- src/pages/NewPost.vue
+++ src/pages/NewPost.vue
@@ -11,31 +11,58 @@ export default {
             action: store.apiUrl + "post",
             title: "",
             content: "",
-            published: ""
+            published: "",
+            tags: [],
+            category:"",
+            success:false
         }
     },
     methods: {
-        getApi(endpoint, data) {
-            axios.post(endpoint, data);
-            this.success = true;
+        async addPost(endpoint, data) {
+            const errors = []
+            await axios.post(endpoint, data)
+            .catch(errorMessages => errorMessages.response.data.errors.forEach(error => errors.push(error)));
+
+            if (errors.length != 0) {
+                 this.success = false;
+            } else {
+                 this.success = true;
+            }
         },
         getForm() {
             const data = {
                 title: this.title,
                 content: this.content,
                 published: this.published,
-                tags: [1],
-                categoryId: 1
+                tags: this.tags,
+                categoryId: this.category
             }
-            this.getApi(store.apiUrl + "/posts", data);
+            this.addPost(store.apiUrl + "/posts", data);
+        },
+        getTags(endpoint) {
+            axios.get(endpoint)
+            .then(res => {
+                store.tags = res.data;
+                
+            })
+        },
+        getCategories(endpoint) {
+            axios.get(endpoint)
+            .then(res => {
+                store.categories = res.data;
+            })
         }
+    },
+    mounted() {
+        this.getTags(store.apiUrl + "/tags");
+        this.getCategories(store.apiUrl + "/categories");
     }
 }
 </script>
 
 <template>
     <div class="container">
-        <div class="container-form">
+        <div v-if="!this.success" class="container-form">
             <h1>New Post</h1>
             <form>
                 <div class="mb-3">
@@ -47,24 +74,45 @@ export default {
                     <label for="content" class="form-label">Content</label>
                     <input v-model="this.content" type="content" class="form-control" id="content" />
                 </div>
-    
-                <div class="form-check">
-                  <input class="form-check-input" v-model="this.published" v-bind:value="'true'" type="radio" name="flexRadioDefault" id="true" checked>
-                  <label class="form-check-label" for="true">
-                    Pubblicato
-                  </label>
+
+                <div class="mb-3">
+                    <div class="form-check">
+                      <input class="form-check-input" v-model="this.published" v-bind:value="'true'" type="radio" name="flexRadioDefault" id="true">
+                      <label class="form-check-label" for="true">
+                        Pubblicato
+                      </label>
+                    </div>
+                    <div class="form-check">
+                      <input class="form-check-input" v-model="this.published" v-bind:value="'false'" type="radio" name="flexRadioDefault" id="false">
+                      <label class="form-check-label" for="false">
+                        Non pubblicato
+                      </label>
+                    </div>
                 </div>
-                <div class="form-check">
-                  <input class="form-check-input" v-model="this.published" v-bind:value="'false'" type="radio" name="flexRadioDefault" id="false">
-                  <label class="form-check-label" for="false">
-                    Non pubblicato
-                  </label>
-                </div>
     
+                <div class="mb-3">
+                    <select v-model="this.category" class="form-select" aria-label="Default select example">
+                        <option selected>Seleziona la categoria</option>
+                        <option v-for="category in store.categories" :key="category.id" :value="category.id">{{category.name}}</option>
+                    </select>
+                </div>
+
+                <div class="mb-3 d-flex flex-wrap justify-content-center gap-3">
+                    <div v-for="tag in store.tags" :key="tag.id" class="form-check">
+                      <input v-model="this.tags" v-bind:value="tag.id" class="form-check-input" type="checkbox" :value="tag.id" id="flexCheckDefault">
+                      <label class="form-check-label" for="flexCheckDefault">
+                        {{tag.name}}
+                      </label>
+                    </div>
+                </div>
+                
                 </form>
-            <button @click="this.getForm"  class="btn btn-primary button">Submit</button>
+            <button @click="this.getForm" class="btn btn-primary button">Submit</button>
         </div>
-    </div>
+        <div v-if="this.success" class="success">
+            <h1>Post creato con successo</h1>
+        </div>
+</div>
 </template>
 
 
