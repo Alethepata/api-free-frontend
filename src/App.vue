e30ee483713d986ccd795aa487bf640965676e2a
Alethepata <alessia.patane@gmail.com>
2024-06-11 12:40:48 +0200

    edit container

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ export default {
 
 </script>
 <template>
-  <div class="container">
+  <div class="container-custom">
     <RouterView />
   </div>
 </template>
 
 <style>
-
+.container-custom{
+  width: 50%;
+  margin: 0 auto;
+  border: 1px solid black;
+}
 </style>
